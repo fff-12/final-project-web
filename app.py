24822b8f0e146cf164899f5a65b2367de9edb7f6
TwoBlyadi <blabladina@gmail.com>
2025-04-23 19:18:23 +0300

    Шаблон

--- app.py
+++ app.py
@@ -2,9 +2,25 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return render_template("web.html")
+    return render_template('web.html')
 
-if __name__ == "__main__":
+@app.route('/magic')
+def magic():
+    return render_template('magic.html')
+
+@app.route('/melee')
+def meele():
+    return render_template('melee.html')
+
+@app.route('/ranger')
+def ranger():
+    return render_template('ranger.html')
+
+@app.route('/summoner')
+def summoner():
+    return render_template('summoner.html')
+
+if __name__ == '__main__':
     app.run(debug=True)
